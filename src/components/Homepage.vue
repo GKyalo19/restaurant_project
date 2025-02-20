5b29ae87ef2b7c86e560d49cd9c53606739b4d02
Grace Kyalo <kyalograce21@gmail.com>
2025-02-20 12:20:44 +0300

    working on menu

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,8 +3,8 @@
     <v-card class="mx-auto" elevation="24">
 
       <v-carousel hide-delimiters>
-          <v-carousel-item src="../public/ivan-stern-LOLSb7m6XkU-unsplash.jpg" cover> </v-carousel-item>
-          <v-carousel-item src="../public/jay-wennington-N_Y88TWmGwA-unsplash.jpg" cover> </v-carousel-item>
+          <v-carousel-item src="/ivan-stern-LOLSb7m6XkU-unsplash.jpg" cover> </v-carousel-item>
+          <v-carousel-item src="/jay-wennington-N_Y88TWmGwA-unsplash.jpg" cover> </v-carousel-item>
           <v-carousel-item src="../public/patrick-tomasso-GXXYkSwndP4-unsplash.jpg" cover> </v-carousel-item>
           <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover> </v-carousel-item>
       </v-carousel>
@@ -17,7 +17,7 @@
             <v-row>
                 <v-col cols="12" md="4">
                     <v-card>
-                        <v-card color="yellow">
+                        <v-card color="teal-darken-4">
                             <v-card-item>
                                 <v-card-title>Special delicacies!</v-card-title>
 
@@ -29,15 +29,15 @@
                               Creamy mild coconut chicken curry served with steamed vegetables and mango chutney. With your choice of rice, fries or mashed potatoes.
                             </v-card-text>
                         </v-card>
-                        <v-card color="yellow">
+                        <v-card color="teal-darken-4">
                             <v-card-item>
 
-                            <v-card-subtitle>Hawaiian Barbeque Burger</v-card-subtitle>
+                            <v-card-subtitle>Nyama Feast Pizza</v-card-subtitle>
                             </v-card-item>
-                            <v-img height="300px" src="../public/jonathan-borba-8l8Yl2ruUsg-unsplash.jpg" cover ></v-img>
+                            <v-img height="400px" src="../public/pexels-enginakyurt-2619970.jpg" cover ></v-img>
 
                             <v-card-text>
-                              Chicken Breast/Beef Patty with Beef Macon, grilled pineapple, fresh onion rings, tomatoes, shredded lettuce, melted cheese, Hawaiian BBQ sauce with ranch dressing.
+                              Beef Sausages, beef strips, chicken cubes, tomato and mozzarella cheese 
                             </v-card-text>
                         </v-card>
                     </v-card>
@@ -46,7 +46,7 @@
 
                 <v-col cols="12" md="4">
                     <v-card>
-                    <v-card color="yellow">
+                    <v-card color="teal-darken-4">
                     <v-card-item>
                     <v-card-title>Upcoming Events</v-card-title>
 
@@ -58,12 +58,12 @@
                     Get ready to belt out your favorite tunes at our upcoming Karaoke Night! Join us for an evening of fun, laughter, and musical mayhem as we take the stage to sing our hearts out!
                     </v-card-text>
                     </v-card>
-                    <v-card color="yellow">
+                    <v-card color="teal-darken-4">
                     <v-card-item>
                     
                       <v-card-subtitle>Mini-concert</v-card-subtitle>
                     </v-card-item>
-                    <v-img height="300px" src="../public/njerae concert.jpg" cover ></v-img>
+                    <v-img height="400px" src="../public/njerae concert.jpg" cover ></v-img>
 
                     <v-card-text>
                       Join us for an intimate evening of live music featuring Njerae 🤩, performing a selection of her most beloved hits in a cozy, relaxed setting at Kwetu Dines!
@@ -75,28 +75,26 @@
 
                 <v-col cols="12" md="4">
                     <v-card>
-                        <v-card color="yellow">
+                        <v-card color="teal-darken-4">
                     <v-card-item>
                     <v-card-title>Promotions</v-card-title>
 
-                    <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+                    <v-card-subtitle>Hawaiian Barbeque Burger</v-card-subtitle>
                     </v-card-item>
-                    <v-img height="200px" src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover ></v-img>
+                    <v-img height="300px" src="../public/jonathan-borba-8l8Yl2ruUsg-unsplash.jpg" cover ></v-img>
 
                     <v-card-text>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+                      Chicken Breast/Beef Patty with Beef Macon, grilled pineapple, fresh onion rings, tomatoes, shredded lettuce, melted cheese, Hawaiian BBQ sauce with ranch dressing.
                     </v-card-text>
                         </v-card>
-                        <v-card color="yellow">
+                        <v-card color="teal-darken-4">
                     <v-card-item>
-                    <v-card-title>Promotions</v-card-title>
-
-                    <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+                    <v-card-subtitle>Banana split Milkshake</v-card-subtitle>
                     </v-card-item>
-                    <v-img height="200px" src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover ></v-img>
+                    <v-img height="400px" src="../public/Ultimate Banana Split Milkshakes Recipe Guide.jpg" cover ></v-img>
 
                     <v-card-text>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+                      A luscious blend of ripe bananas, velvety vanilla ice cream, and creamy milk, swirled together for the perfect thick and smooth shake!
                     </v-card-text>
                         </v-card>
                     </v-card>
@@ -104,6 +102,47 @@
                 </v-col>
             </v-row>
         </v-container>
+        </v-card>
+    </v-container>
+
+    <v-container>
+        <v-card class="mx-auto" elevation="24" color = "teal-accent-3">
+            <v-container>
+                <v-row >
+                    <v-col cols="12" md="4">
+                        <v-card>
+                            <v-card-item>
+                                <v-card-title>Contact Us</v-card-title>
+                            </v-card-item>
+                            <v-card-text><v-icon icon="mdi-phone" /> 0722123456</v-card-text>
+                            <v-card-text><v-icon icon="mdi-email" /> myrestaurant@example.com</v-card-text>
+                            <v-card-text>Operating Hours: 10.00am - 10.00pm</v-card-text>
+                        </v-card>
+                    </v-col>
+
+                        <v-col cols="12" md="4">
+                        <v-card>
+                            <v-card-item>
+                            <v-card-title>Find us on our socials</v-card-title>
+
+                            </v-card-item>
+                            <v-card-text><v-icon icon="mdi-instagram" /> myrestaurant</v-card-text>
+                            <v-card-text><v-icon icon="mdi-twitter" /> myrestaurant</v-card-text>
+                            <v-card-text><v-icon icon="mdi-facebook" /> myrestaurant</v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                        <v-card>
+                            <v-card-item>
+                            <v-card-title>Our Restaurant Locations</v-card-title>
+                            </v-card-item>
+                            <v-card-text>Madaraka</v-card-text>
+                            <v-card-text>CBD</v-card-text>
+                            <v-card-text>Langata</v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-card>
     </v-container>
 
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,100 @@
 <template>
-    <h1>Menu</h1>
+   <v-container>
+    <v-card class="mx-auto" elevation="24">
+        <v-card color="blue" >
+            <v-card-title>
+                <div class="text-h3">Our Menu</div>
+            </v-card-title>
+        </v-card>
+    
+    <v-container>
+        <v-card class="mx-auto" elevation="24" color = "brown-darken-2">
+            <v-container>
+                <v-row >
+                    <v-col cols="12" md="4" v-for="item in menuItems" :key="item">
+                        <v-card color="teal-lighten-3">
+                          <v-card-item>
+                            <v-card-title>{{ item.name }}</v-card-title>
+                            <v-card-subtitle>{{ item.description }}</v-card-subtitle>
+                            <v-card-text>Price: {{ item.price }}</v-card-text>
+                          </v-card-item>
+                        </v-card>
+                      </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
+    </v-container>
+  </v-card>
+  </v-container>
+
   </template>
   
   <script setup>
+    const menuItems = [
+    {
+        name: "Chips",
+        price: 200,
+        description: "Best chips in town",
+        size: "Large",
+        offers: "None",
+        image: "https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+    },
+    {
+        name: "Sausage",
+        price: 50,
+        description: "Best sausages in town",
+        size: "None",
+        offers: "None",
+        image: "https://unsplash.com/photos/sausage-on-black-round-pan-cSxpCQrRlo8"
+    },
+    {
+        name: "Chocolate chip muffin",
+        price: 300,
+        description: "Delicious muffin flavored with chocolate drops.",
+        size: "None",
+        offers: "None",
+        image: "/chocolate chip muffin.jpg"
+    },
+    {
+        name: "Original Buttermilk Pancake",
+        price: 900,
+        description: "Best buttermilk pancakes in town!",
+        size: "None",
+        offers: "None",
+        image: "/Buttermilk Pancake.png"
+    },
+    {
+        name: "Butternut squash soup",
+        price: 550,
+        description: "Fresh homemade soup prepared for each day of the week. Accompanied with two bread rolls and butter.",
+        size: "None",
+        offers: "None",
+        image: "/butternut squash soup.jpg"
+    },
+    {
+        name: "Beef Burger",
+        price: 1300,
+        description: "Perfectly grilled beef patty with grilled onions, fresh tomatoes, mayonnaise and shredded lettuce.",
+        size: "None",
+        offers: "None",
+        image: "/beef burger.jpg"
+    },
+    {
+        name: "Samosas",
+        price: 350,
+        description: "A pair of chicken or beef or vegetable deep fried stuffed pastry shells.",
+        size: "None",
+        offers: "None",
+        image: "/samosas.jpg"
+    },
+    {
+        name: "Masala chips",
+        price: 500,
+        description: "Chips tossed with fresh coriander, home made masala sauce made of onions, cumin seeds, curry paste, tomato sauce and fresh cream.",
+        size: "None",
+        offers: "None",
+        image: "/masala chips.jpg"
+    },
+]
   </script>
   
